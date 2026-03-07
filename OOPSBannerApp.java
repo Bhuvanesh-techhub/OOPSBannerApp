72466b91ffa2f0d54f1ed50e40a23bba2bf31803
Bhuvanesh-techhub <bhuvaneshb.cs@gmail.com>
2026-03-07 08:55:55 +0530

    UC8 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,75 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        Map<Character, String[]> patternMap = buildCharacterPatterns();
 
-        lines[0] = String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", " *   * ", " *   * ", " *   * ", " *     ");
-        lines[2] = String.join(" ", " *   * ", " *   * ", " ***** ", " ***** ");
-        lines[3] = String.join(" ", " *   * ", " *   * ", " *     ", "     * ");
-        lines[4] = String.join(" ", " *   * ", " *   * ", " *     ", "     * ");
-        lines[5] = String.join(" ", " *   * ", " *   * ", " *     ", " *   * ");
-        lines[6] = String.join(" ", "  ***  ", "  ***  ", " *     ", " ***** ");
+        String message = "OOPS";
 
-        for (String line : lines) {
+        renderBanner(message, patternMap);
+    }
+
+    // Method to create and store character patterns
+    public static Map<Character, String[]> buildCharacterPatterns() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        String[] O = {
+                "  ***  ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                "  ***  "
+        };
+
+        String[] P = {
+                " ***** ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+
+        String[] S = {
+                " ***** ",
+                " *     ",
+                " ***** ",
+                "     * ",
+                "     * ",
+                " *   * ",
+                " ***** "
+        };
+
+        map.put('O', O);
+        map.put('P', P);
+        map.put('S', S);
+
+        return map;
+    }
+
+    // Method to display banner
+    public static void renderBanner(String message, Map<Character, String[]> map) {
+
+        int rows = 7;
+
+        for (int i = 0; i < rows; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = map.get(ch);
+
+                line.append(pattern[i]).append(" ");
+            }
+
             System.out.println(line);
         }
     }
